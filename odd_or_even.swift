be3117c6837d3cf3f47ddc2e8ceecd22b50e6e4e
Jerald Jacob <jeraldjacob10@gmail.com>
2020-05-21 18:34:00 +0530

    Create odd_or_even.swift

---
+++ odd_or_even.swift
@@ -0,0 +1,10 @@
+// swift Program to check the given number is Even or Odd
+
+
+var inputNumber:Int = 57
+if ((inputNumber % 2) == 0){
+    print("the give number is even")
+}
+else{
+    print("it's odd")
+}
